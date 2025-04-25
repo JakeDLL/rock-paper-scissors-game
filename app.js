89fc0b7a6227353bd9dd47e8c0d6ddfecf6698b5
JakeDLL <35861176+JakeDLL@users.noreply.github.com>
2025-04-24 22:09:00 -0400

    Remove: code that will never be reached.
    Already stated return values. No need for break statements

--- app.js
+++ app.js
@@ -8,13 +8,10 @@ function getComputerChoice() {
 	switch (randNum) {
 		case 1:
 			return 'rock';
-			break;
 		case 2:
 			return 'paper';
-			break;
 		case 3:
 			return 'scissors';
-			break;
 		default:
 			return randNum;
 	}
